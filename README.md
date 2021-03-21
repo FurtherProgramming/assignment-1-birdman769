d1b3e5c7620cd3aa8a9e309bd7661e7961fc2e70
Coding Rooms <git@codingrooms.io>
2021-03-21 11:01:09 +0000

    created test book unit test

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # assignment-1-Matthew Bird s3482450 (birdman769)
+
+#made using:
+#mvn clean
+#mvn test
+#mvn compile
+#mvn package
+#java -cp target/my-app-1.0-SNAPSHOT.jar com.DainTree.app.App
+
+
+#also runs using:
+#mvn compile
+#mvn exec:java -Dexec.mainClass=com.DainTree.app.App
--- my-app/src/test/java/com/DainTree/app/AppTest.java
+++ my-app/src/test/java/com/DainTree/app/AppTest.java
@@ -1,6 +1,7 @@
 package com.DainTree.app;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 
@@ -13,8 +14,19 @@ public class AppTest
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testBook() //test that our getters for our book objects are working 
+    {   Book book = new Book("testTitle","testAuthor",50.00,8.00, 5, true);
+        assertEquals("testTitle", book.getTitle());
+        assertEquals("testAuthor", book.getAuthor());
+        assertEquals(50.00, book.getHardPrice(), 0.001);
+        assertEquals(8.00, book.getEPrice(), 0.001);
+        assertEquals(5, book.getStock());
+        assertEquals(true, book.isDigital());
+    }
+    @Test
+     public void dummy()
+    {   
+        
+        assertEquals(5, 5);
     }
 }
